9211f0f2161b6b587a5c7afd50320200df8bf8ee
root <root@cos4>
2015-01-14 00:11:38 -0800

    学习位运算，补码等知识。

---
+++ src/javalanguage/classloader/获取当前Java类所在路径研究.java
@@ -0,0 +1,17 @@
+package javalanguage.classloader;
+
+
+public class 获取当前Java类所在路径研究 {
+
+	public static void main(String[] args) {
+
+		System.out.println(获取当前Java类所在路径研究.class.getResource(""));
+		System.out.println(获取当前Java类所在路径研究.class.getResource("/"));	
+		System.out.println(Thread.currentThread().getContextClassLoader().getResource(""));
+		System.out.println(Thread.currentThread().getContextClassLoader().getResource(""));
+		System.out.println(获取当前Java类所在路径研究.class.getClassLoader().getResource(""));
+		System.out.println(ClassLoader.getSystemResource(""));
+
+	}
+
+}
