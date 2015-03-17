15b03eb15d7a213630edc43ee00ac2070c922720
miscellanea <miscellanea@yeah.net>
2015-03-17 12:02:03 +0800

    learning shift operators

--- src/javalanguage/运算符/位运算.java
+++ src/javalanguage/运算符/位运算.java
@@ -25,13 +25,57 @@ public class 位运算 {
 		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 		int n = -1;//1111 1111 1111 1111 1111 1111 1111 1111
 		System.out.println(n >>> 1);//0111 1111 1111 1111 1111 1111 1111 1111
+		System.out.println(n >>> 2);//0011 1111 1111 1111 1111 1111 1111 1111
+		
+		System.out.println("~~~~~~~~~~~~~~~222~~~~~~~~~~~~~~~~~~~~");
 		System.out.println(n >> 1);//1111 1111 1111 1111 1111 1111 1111 1111
+		System.out.println(n >> 23);//1111 1111 1111 1111 1111 1111 1111 1111
+		System.out.println(n >> 30);//1111 1111 1111 1111 1111 1111 1111 1111
+		
+		System.out.println(n << 1);//1111 1111 1111 1111 1111 1111 1111 1110
+		System.out.println(n << 2);//1111 1111 1111 1111 1111 1111 1111 1100
+		System.out.println(n << 3);//1111 1111 1111 1111 1111 1111 1111 1000
+	
+		System.out.println("~~~~~~~~~~~~~~~333~~~~~~~~~~~~~~~~~~~~");		
+		int b2 = 0xbffffffd;
+		System.out.println(b2);//1011 1111 1111 1111 1111 1111 1111 1101
+		System.out.println(b2>>>1);//0101 1111 1111 1111 1111 1111 1111 1110
+		System.out.println(b2>>1);//1101 1111 1111 1111 1111 1111 1111 1110
+		System.out.println(0x5ffffffe);//0101 1111 1111 1111 1111 1111 1111 1110
+		System.out.println(0xdffffffe);//1101 1111 1111 1111 1111 1111 1111 1110
+		
+		System.out.println(b2>>>2);//0010 1111 1111 1111 1111 1111 1111 1111
+		System.out.println(b2>>2);//1110 1111 1111 1111 1111 1111 1111 1111
+		System.out.println(0x2fffffff);//0010 1111 1111 1111 1111 1111 1111 1111
+		System.out.println(0xefffffff);//1110 1111 1111 1111 1111 1111 1111 1111		
+		System.out.println("~~~~~~~~~~~~~~~444~~~~~~~~~~~~~~~~~~~~");
+		
+		int positiveMaxInt = Integer.MAX_VALUE;//0111 1111 1111 1111 1111 1111 1111 1111
+		System.out.println(positiveMaxInt);
+		System.out.println(positiveMaxInt << 1);//1111 1111 1111 1111 1111 1111 1111 1110
+		System.out.println(0xfffffffe);//1111 1111 1111 1111 1111 1111 1111 1110
+		
+		int int2 = 0x4fffffff;//0100 1111 1111 1111 1111 1111 1111 1111
+		System.out.println(int2);
+		System.out.println(int2 << 1);//1001 1111 1111 1111 1111 1111 1111 1110
+		System.out.println(0x9ffffffd);//1001 1111 1111 1111 1111 1111 1111 1110
+		System.out.println(int2 << 2);//0011 1111 1111 1111 1111 1111 1111 1100
+		System.out.println(0x3ffffffc);//0011 1111 1111 1111 1111 1111 1111 1100
 		
 		int n1 = 4;//0000 0000 0000 0000 0000 0000 0000 0100
 		int n2 = -4;//1111 1111 1111 1111 1111 1111 1111 1100
 		System.out.println(n1<<1);//0000 0000 0000 0000 0000 0000 0000 1000
 		System.out.println(n2<<1);//1111 1111 1111 1111 1111 1111 1111 1000
-		
+		System.out.println("~~~~~~~~~~~~~~~555~~~~~~~~~~~~~~~~~~~~");		
+		System.out.println(n1<<1);//0000 0000 0000 0000 0000 0000 0001 0000
+		System.out.println(n1<<2);//0000 0000 0000 0000 0000 0000 0010 0000
+		System.out.println(n1<<3);//0000 0000 0000 0000 0000 0000 0100 0000
+		System.out.println("~~~~~~~~~~~~~~~666~~~~~~~~~~~~~~~~~~~~");	
+		System.out.println(n2>>1);//1111 1111 1111 1111 1111 1111 1111 1000
+		System.out.println(n2>>2);//1111 1111 1111 1111 1111 1111 1111 1000
+		System.out.println(n2>>3);//1111 1111 1111 1111 1111 1111 1111 1000
+
+		System.out.println("*********************************************");
 		int n3 = 1073741828;//0100 0000 0000 0000 0000 0000 0000 0100
 		System.out.println(n3<<1);//1000 0000 0000 0000 0000 0000 0000 1000
 		
