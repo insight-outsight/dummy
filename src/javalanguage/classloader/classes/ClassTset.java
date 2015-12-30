55ee786a4605e4dcb64058716c282448726556d1
miscellanea <miscellanea@yeah.net>
2015-12-30 19:03:19 +0800

    增加 Class和Executors学习用方法

--- src/javalanguage/classloader/classes/ClassTset.java
+++ src/javalanguage/classloader/classes/ClassTset.java
@@ -13,6 +13,21 @@ public class ClassTset {
 //		System.out.println("5."+Number.class.asSubclass(Integer.class));//ClassCastException
 
 
+		int[] ae = new int[0];
+		Class<?> aeClass = ae.getClass();
+		System.out.println(aeClass.isArray());//判断类是否是数据，打印true
+		System.out.println(aeClass.getComponentType());//获取代表数组的类中数组元素的类型，打印int
+		System.out.println("--------y--------");
+		Integer[] ae2 = new Integer[1];
+		Class<?> ae2Class = ae2.getClass();
+		System.out.println(ae2Class.isArray());//打印true
+		System.out.println(ae2Class.getComponentType());//打印class java.lang.Integer
+		System.out.println("--------xx--------");
+		String s = new String("wytw");
+		Class<?> sClass = s.getClass();
+		System.out.println(sClass.isArray());//打印false
+		System.out.println(sClass.getComponentType());//打印null
+		
 	}
 
 }
