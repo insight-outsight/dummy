b0919f5dd7eedb39d78f6b69adae54cd6b7a6621
u0007 <ccdeveloper01@sina.com>
2017-07-24 13:06:26 +0800

    厉害了

--- src/javalanguage/util/cocurrent/ExecutorsDemo.java
+++ src/javalanguage/util/cocurrent/ExecutorsDemo.java
@@ -2,18 +2,22 @@ package javalanguage.util.cocurrent;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class ExecutorsDemo {
 
 	public static void main(String[] args) throws InterruptedException {
-		int poolSize = 5;
-		ExecutorService pool = Executors.newFixedThreadPool(3);  
+		int poolSize = 2;
+//		ExecutorService pool = Executors.newFixedThreadPool(poolSize);  
+		ExecutorService pool = new ThreadPoolExecutor(poolSize, poolSize, 
+				0L, TimeUnit.MILLISECONDS,new LinkedBlockingQueue<Runnable>(1));
 
 		final AtomicInteger ai = new AtomicInteger(0);
 		
-        for(int i=0;i<poolSize;i++){
+        for(int i=0;i<poolSize+2;i++){
         	
             pool.execute(new Runnable(){
 
@@ -21,16 +25,22 @@ public class ExecutorsDemo {
 				public void run() {
 					int currentRunnerNum = ai.getAndIncrement();
 					for(int j=0;j<Integer.MAX_VALUE;j++){
-				        System.out.println(j+"."+Thread.currentThread().getName()+"****" +currentRunnerNum+ "***正在执行。。。"); 
-						try {
-							TimeUnit.MILLISECONDS.sleep(2000);
-							if(j==4)return;
-						} 
-						catch (InterruptedException e) {
-							System.out.println("我被中断，但我不响应，继续运行。");
-							e.printStackTrace();
-//							System.out.println("我被中断，我响应，结束运行。");
-//							break; 
+						if (!Thread.interrupted()) { 
+					        System.out.println(j+"."+Thread.currentThread().getName()+"****" +currentRunnerNum+ "***正在执行。。。"); 
+							try {
+								TimeUnit.MILLISECONDS.sleep(2000);
+	//							if(j==4)return;
+							} 
+							catch (InterruptedException e) {
+//								System.out.println("我被中断，但我不响应，继续运行。");
+								e.printStackTrace();
+								System.out.println("我被中断，我响应，结束运行。");
+	//							break; 
+								Thread.currentThread().interrupt(); 
+							}
+						}
+						else{
+							break;
 						}
 					}
 				}
@@ -38,19 +48,24 @@ public class ExecutorsDemo {
             });  
 
         }
+
+
+        Runtime.getRuntime().addShutdownHook(new Thread(){
+        	public void run() {  
+                try {  
+                    Thread.sleep(2000);  
+                } catch (InterruptedException e) {  
+                    e.printStackTrace();  
+                }  
+                System.out.println("shutdown hook thread end.");  
+            }
+        });
+        
+        Thread.sleep(1000);  
+
         pool.shutdown();
-        pool.awaitTermination(2000, TimeUnit.MILLISECONDS);
 //        pool.shutdownNow();
-//        Runtime.getRuntime().addShutdownHook(new Thread(){
-//        	public void run() {  
-//                try {  
-//                    Thread.sleep(2000);  
-//                } catch (InterruptedException e) {  
-//                    e.printStackTrace();  
-//                }  
-//                System.out.println("shutdown hook thread end.");  
-//            }
-//        });
+        pool.awaitTermination(2000, TimeUnit.MILLISECONDS);
 //        System.out.println(Thread.currentThread().getName() + "主程序退出。。。");  
 //        System.exit(0);
         System.out.println("fffffffffff");  
