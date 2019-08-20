1fb6778ce941774c3990b285e61fac7723e5b299
xuzhengchao <xuzhengchao@172.18.104.159>
2019-08-20 16:46:03 +0800

    ServiceLoader demo

---
+++ src/javalanguage/service_loader/Aab.java
@@ -0,0 +1,19 @@
+package javalanguage.service_loader;
+
+public class Aab {
+
+    private String name;
+    
+    public Aab() {
+        super();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
